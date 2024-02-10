6d2316adfbd5816d0b11973465cd7c3f674a849b
Christian <howelchr@byu.edu>
2024-02-10 14:04:00 -0700

    added second solution

--- prob1.py
+++ prob1.py
@@ -1,11 +1,21 @@
 """Problem 1
 Find the sum of all the multiples of 3 or 5 below 1000"""
+import numpy as np 
+import time
 
-def sum_fizz_buzz_multiples(a=3, b=5, n=1000):
+def solution1():
     # Sum integers divisible by 3 and 5
-    final_sum = sum([i if i % a == 0 or i % b == 0 else 0 for i in range(n)])
+    final_sum = sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0])
     return final_sum
+
+def solution2():
+    # Sum integers divisible by 3 and 5
+    threes = 3*np.arange(334)
+    fives = 5*np.arange(200)
+
+    return sum(set(np.concatenate([threes, fives])))
 
 
 if __name__=='__main__':
-    print(sum_fizz_buzz_multiples())
+    print(solution1())
+    print(solution2())
