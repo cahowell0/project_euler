f85c53322b100afec881215a8c2a537465372775
Christian <howelchr@byu.edu>
2024-02-09 18:31:07 -0700

    problem 1

---
+++ prob1.py
@@ -0,0 +1,10 @@
+"""Problem 1
+Find the sum of all the multiples of 3 or 5 below 1000"""
+
+def sum_multiples(a=3, b=5, n=1000):
+    final_sum = sum([i if i % a == 0 or i % b == 0 else 0 for i in range(n)])
+    return final_sum
+
+
+if __name__=='__main__':
+    print(sum_multiples())
