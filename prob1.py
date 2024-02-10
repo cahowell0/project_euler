1bcacc0acfc4573dd14ba2d86bcf099f3c3fed55
Christian <howelchr@byu.edu>
2024-02-09 18:51:53 -0700

    problem 1

--- prob1.py
+++ prob1.py
@@ -1,10 +1,11 @@
 """Problem 1
 Find the sum of all the multiples of 3 or 5 below 1000"""
 
-def sum_multiples(a=3, b=5, n=1000):
+def sum_fizz_buzz_multiples(a=3, b=5, n=1000):
+    # Sum integers divisible by 3 and 5
     final_sum = sum([i if i % a == 0 or i % b == 0 else 0 for i in range(n)])
     return final_sum
 
 
 if __name__=='__main__':
-    print(sum_multiples())
+    print(sum_fizz_buzz_multiples())
