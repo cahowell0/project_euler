f78faee5ffb0ec348b696471105b24146952f243
Christian <howelchr@byu.edu>
2024-03-02 15:22:29 -0700

    added second solution

--- prob2.py
+++ prob2.py
@@ -25,7 +25,37 @@ def solution1():
     return even_fib_nums
 
 
+def solution2():
+    # Find sum of even Fibonacci numbers less than 4 million
+    a = 1
+    b = 2
+
+    c = a + b   # This will be odd, so we don't want to include it
+    d = c + c + b   # Even term, so include this one in the final sum
+    fib_num_sum = b + d
+
+    while True:
+        fib_num = 2*(d + c + b) + d   # This skips two Fibonacci numbers, essentiall ignoring the odds
+        
+        if fib_num < 2e6:
+            fib_num_sum += fib_num   # Update sum of even Fibonacci numbers
+            break
+
+        # Update Fibonacci numbers, skipping the odd terms
+        c = d + c + b
+        b = d
+        d = fib_num
+
+    return fib_num_sum
+
 if __name__=='__main__':
+    time1 = time.perf_counter()
     print(solution1())
+    time2 = time.perf_counter()
+    print(solution2())
+    time3 = time.perf_counter()
+
+    print(time2 - time1)
+    print(time3 - time2)
 
     
